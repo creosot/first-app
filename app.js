29d8410fbb32c7a5bde2ab9105217df02fac0016
creosot <creosot@yandex.ru>
2014-08-11 09:49:33 +0400

    xps1

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // web.js
-var express = require("express");
+/*var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
@@ -13,4 +13,23 @@ app.get('/', function(req, res) {
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
     console.log("Listening on " + port);
-});
+});*/
+
+var net = require("net");
+
+var port = Number(process.env.PORT || 3000);
+net.createServer(function(socket){
+    console.log("client connected!!!!");
+
+    socket.on("data", function(data){
+        console.log("server receive: " + data.toString("utf8"));
+        socket.write(data);
+    });
+
+    socket.on("error", function(ex){
+        console.log(ex);
+    });
+}).
+    listen(port, function(){
+        console.log("Socket Listening on " + port);
+    });
